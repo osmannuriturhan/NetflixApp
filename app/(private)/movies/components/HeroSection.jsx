eec559c69299e93ba52eb901128fc27ef714e86e
osmn_turhn <osmannuri.turhan01@gmail.com>
2024-02-01 22:01:01 +0300

    Add HeroSection and VideoSection components

---
+++ app/(private)/movies/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoSection from "./VideoSection";
+
+const HeroSection = () => {
+  return (
+    <div className="relative h-[50vw]">
+      <VideoSection videoKey={videoKey}  />
+    </div>
+  );
+};
+
+export default HeroSection;
--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Movies = async () => {
   const movies = await getMovies("now_playing");
-  return <div></div>;
+  return <></>;
 };
 
 export default Movies;
