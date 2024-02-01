6dba9203e0975cb72251b38df09cc983e7e280c3
osmn_turhn <osmannuri.turhan01@gmail.com>
2024-02-02 00:42:32 +0300

    Add private layout component and movie detail page

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,9 +1,16 @@
 import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
+
+export const metadata = {
+  title: "Movies",
+  description: "Movie page",
+};
 
 const Movies = async () => {
   const movies = await getMovies("now_playing");
+
   return (
     <>
       <HeroSection
@@ -11,6 +18,12 @@ const Movies = async () => {
         title={movies[0].title}
         overview={movies[0].overview}
       />
+      <div className="px-4 md:px-12 mt-4">
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+        <MovieSection title="POPULAR" type="popular" />
+        <MovieSection title="TOP RATED" type="top_rated" />
+        <MovieSection title="UPCOMİNG" type="upcoming" />
+      </div>
     </>
   );
 };
