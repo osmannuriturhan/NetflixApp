bd2d148bb6724ac8ef4bca2311af9fb62e8c9055
osmn_turhn <osmannuri.turhan01@gmail.com>
2024-02-01 22:33:49 +0300

    Add HeroSection component and fetch movie data in Movies page

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,9 +1,18 @@
 import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
+import HeroSection from "./components/HeroSection";
 
 const Movies = async () => {
   const movies = await getMovies("now_playing");
-  return <></>;
+  return (
+    <>
+      <HeroSection
+        movieId={movies[0].id}
+        title={movies[0].title}
+        overview={movies[0].overview}
+      />
+    </>
+  );
 };
 
 export default Movies;
